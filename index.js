724f011ac7a847f6c7b10fd3be82a0dee4b9d175
Bagath <132096909+Bagathexe@users.noreply.github.com>
2024-09-19 16:29:26 +0530

    Changes

---
+++ index.js
@@ -0,0 +1,13 @@
+const display =Document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(input){
+    
+}
+
+function calculate(){
+    
+}
